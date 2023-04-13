1e31e3aa6e90a0edc386a8035a4a5fcceaa2bdef
SrRapero720 <SrRapero720@hotmail.com>
2023-04-13 15:09:43 -0600

    Removed x86 support (Java 17 only supports x64)

--- src/main/java/me/srrapero720/waterframes/vlc/LocalFileDiscoveryDirectoryProvider.java
+++ src/main/java/me/srrapero720/waterframes/vlc/LocalFileDiscoveryDirectoryProvider.java
@@ -8,7 +8,7 @@ import uk.co.caprica.vlcj.binding.RuntimeUtil;
 import uk.co.caprica.vlcj.factory.discovery.provider.DiscoveryDirectoryProvider;
 
 @Deprecated(forRemoval = false)
-// I remove this because FancyVideo-API unwrapp their vlc on game dir, and my cpde try to use it (and have broken things)
+// I remove this because FancyVideo-API unwrap their vlc on game dir, and my cpde try to use it (and have broken things)
 public class LocalFileDiscoveryDirectoryProvider implements DiscoveryDirectoryProvider {
     
     @Override
@@ -24,14 +24,9 @@ public class LocalFileDiscoveryDirectoryProvider implements DiscoveryDirectoryPr
     @Override
     public String[] directories() {
         Path vlc = FMLPaths.GAMEDIR.get().resolve("vlc");
-        if (RuntimeUtil.isNix())
-            vlc = vlc.resolve("linux");
-        else if (RuntimeUtil.isMac())
-            vlc = vlc.resolve("mac");
-        else {
-            boolean is64 = System.getProperty("sun.arch.data.model").equals("64");
-            vlc = vlc.resolve("windows_" + (is64 ? "x64" : "x86"));
-        }
+        if (RuntimeUtil.isWindows()) vlc = vlc.resolve("windows_x64");
+        else if (RuntimeUtil.isNix()) vlc = vlc.resolve("linux");
+        else vlc.resolve("mac");
         return new String[] { vlc.toString() };
     }
 }
