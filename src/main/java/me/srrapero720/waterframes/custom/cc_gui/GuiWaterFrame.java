9f57fc3a61d8f04217f73088abb0f68bed59c917
SrRapero720 <SrRapero720@hotmail.com>
2023-04-17 19:16:14 -0600

    Enhanced GUI and removed usages of CreativeCoreConfig

--- src/main/java/me/srrapero720/waterframes/custom/cc_gui/GuiWaterFrame.java
+++ src/main/java/me/srrapero720/waterframes/custom/cc_gui/GuiWaterFrame.java
@@ -1,6 +1,9 @@
 package me.srrapero720.waterframes.custom.cc_gui;
 
-import me.srrapero720.waterframes.WaterFrames;
+import me.srrapero720.waterframes.FramesConfig;
+import me.srrapero720.waterframes.custom.blocks.BlockEntityWaterFrame;
+import me.srrapero720.waterframes.custom.displayers.texture.TextureCache;
+import me.srrapero720.waterframes.custom.displayers.texture.TextureSeeker;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.EndTag;
@@ -19,9 +22,6 @@ import team.creative.creativecore.common.gui.sync.GuiSyncLocal;
 import team.creative.creativecore.common.util.mc.ColorUtils;
 import team.creative.creativecore.common.util.text.TextBuilder;
 import team.creative.creativecore.common.util.text.TextListBuilder;
-import me.srrapero720.waterframes.custom.displayers.texture.TextureCache;
-import me.srrapero720.waterframes.custom.displayers.texture.TextureSeeker;
-import me.srrapero720.waterframes.custom.blocks.BlockEntityWaterFrame;
 
 public class GuiWaterFrame extends GuiLayer {
     
@@ -37,10 +37,10 @@ public class GuiWaterFrame extends GuiLayer {
     
     public final GuiSyncLocal<CompoundTag> SET_DATA = getSyncHolder().register("set_data", nbt -> {
         String url = nbt.getString("url");
-        if (WaterFrames.CONFIG.canUse(getPlayer(), url)) {
+        if (FramesConfig.canUse(getPlayer(), url)) {
             frame.setURL(url);
-            float sizeX = (float) Math.min(WaterFrames.CONFIG.sizeLimitation, nbt.getFloat("x"));
-            float sizeY = (float) Math.min(WaterFrames.CONFIG.sizeLimitation, nbt.getFloat("y"));
+            float sizeX = (float) Math.min(FramesConfig.MAX_SIZE.get(), nbt.getFloat("x"));
+            float sizeY = (float) Math.min(FramesConfig.MAX_SIZE.get(), nbt.getFloat("y"));
             int posX = nbt.getByte("posX");
             int posY = nbt.getByte("posY");
             if (posX == 0) {
@@ -67,7 +67,7 @@ public class GuiWaterFrame extends GuiLayer {
                 frame.max.y = 1;
             }
             
-            frame.renderDistance = Math.min(WaterFrames.CONFIG.maxRenderDistance, nbt.getInt("render"));
+            frame.renderDistance = Math.min(FramesConfig.MAX_RENDER_DISTANCE.get(), nbt.getInt("render"));
             frame.rotation = nbt.getFloat("rotation");
             frame.visibleFrame = nbt.getBoolean("visibleFrame");
             frame.bothSides = nbt.getBoolean("bothSides");
@@ -89,7 +89,7 @@ public class GuiWaterFrame extends GuiLayer {
     }
     
     public GuiWaterFrame(BlockEntityWaterFrame frame, int scaleSize) {
-        super("waterframe", 250, 220);
+        super("waterframe", 250, 200);
         this.frame = frame;
         this.scaleMultiplier = 1F / (scaleSize);
     }
@@ -101,56 +101,56 @@ public class GuiWaterFrame extends GuiLayer {
             GuiTextfield url = get("url");
             GuiCounterDecimal sizeX = get("sizeX");
             GuiCounterDecimal sizeY = get("sizeY");
-            
+
             GuiStateButton buttonPosX = get("posX");
             GuiStateButton buttonPosY = get("posY");
             GuiSlider rotation = get("rotation");
-            
+
             GuiCheckBox flipX = get("flipX");
             GuiCheckBox flipY = get("flipY");
             GuiCheckBox visibleFrame = get("visibleFrame");
             GuiCheckBox bothSides = get("bothSides");
-            
+
             GuiSteppedSlider renderDistance = get("distance");
-            
+
             GuiSlider transparency = get("transparency");
             GuiSlider brightness = get("brightness");
-            
+
             GuiCheckBox loop = get("loop");
             GuiSlider volume = get("volume");
             GuiSteppedSlider min = get("range_min");
             GuiSteppedSlider max = get("range_max");
-            
+
             nbt.putByte("posX", (byte) buttonPosX.getState());
             nbt.putByte("posY", (byte) buttonPosY.getState());
-            
+
             nbt.putFloat("rotation", (float) rotation.value);
-            
+
             nbt.putBoolean("flipX", flipX.value);
             nbt.putBoolean("flipY", flipY.value);
             nbt.putBoolean("visibleFrame", visibleFrame.value);
             nbt.putBoolean("bothSides", bothSides.value);
-            
+
             nbt.putInt("render", (int) renderDistance.value);
-            
+
             nbt.putFloat("transparency", (float) transparency.value);
             nbt.putFloat("brightness", (float) brightness.value);
-            
+
             nbt.putBoolean("loop", loop.value);
             nbt.putFloat("volume", (float) volume.value);
             nbt.putFloat("min", min.getValue());
             nbt.putFloat("max", max.getValue());
-            
+
             nbt.putString("url", url.getText());
             nbt.putFloat("x", Math.max(0.1F, sizeX.getValue()));
             nbt.putFloat("y", Math.max(0.1F, sizeY.getValue()));
             SET_DATA.send(nbt);
         });
-        save.setTranslate("gui.save");
-        
+        save.setTranslate("gui.waterframes.save");
+
         align = Align.STRETCH;
         flow = GuiFlow.STACK_Y;
-        
+
         url = new GuiTextField(save, "url", frame.getRealURL());
         url.setMaxStringLength(512);
         add(url);
@@ -158,19 +158,23 @@ public class GuiWaterFrame extends GuiLayer {
         if (frame.isClient() && frame.cache != null && frame.cache.getError() != null)
             error.setTranslate(frame.cache.getError());
         add(error);
-        
+
+        // SIZE -----------------------
         GuiParent size = new GuiParent(GuiFlow.STACK_X);
+        size.spacing = 8;
         size.align = Align.STRETCH;
         add(size);
-        
-        size.add(new GuiCounterDecimal("sizeX", frame.getSizeX(), 0, Float.MAX_VALUE) {
+        GuiCounterDecimal counterDecimal;
+
+        // LEFT
+        size.add(counterDecimal = new GuiCounterDecimal("sizeX", frame.getSizeX(), 0, Float.MAX_VALUE) {
             @Override
             public float stepUp(float value) {
                 int scaled = (int) (value / scaleMultiplier);
                 scaled++;
                 return Math.min(max, scaled * scaleMultiplier);
             }
-            
+
             @Override
             public float stepDown(float value) {
                 int scaled = (int) (value / scaleMultiplier);
@@ -178,25 +182,28 @@ public class GuiWaterFrame extends GuiLayer {
                 return Math.max(min, scaled * scaleMultiplier);
             }
         });
-        
-        size.add(new GuiButton("reX", but -> {
+        counterDecimal.setExpandableX();
+        counterDecimal.spacing = 2;
+        counterDecimal.align = Align.CENTER;
+        counterDecimal.add(new GuiButton("reX", but -> {
             GuiCounterDecimal sizeXField = get("sizeX", GuiCounterDecimal.class);
             GuiCounterDecimal sizeYField = get("sizeY", GuiCounterDecimal.class);
-            
+
             float x = sizeXField.getValue();
-            
+
             if (frame.display != null)
                 sizeYField.setValue(frame.display.getHeight() / (frame.display.getWidth() / x));
-        }).setTitle(new TextComponent("x->y")));
-        
-        size.add(new GuiCounterDecimal("sizeY", frame.getSizeY(), 0, Float.MAX_VALUE) {
+        }).setTitle(new TextComponent("x->y")).setExpandableY());
+
+        // RIGHT
+        size.add(counterDecimal = new GuiCounterDecimal("sizeY", frame.getSizeY(), 0, Float.MAX_VALUE) {
             @Override
             public float stepUp(float value) {
                 int scaled = (int) (value / scaleMultiplier);
                 scaled++;
                 return Math.min(max, scaled * scaleMultiplier);
             }
-            
+
             @Override
             public float stepDown(float value) {
                 int scaled = (int) (value / scaleMultiplier);
@@ -204,87 +211,109 @@ public class GuiWaterFrame extends GuiLayer {
                 return Math.max(min, scaled * scaleMultiplier);
             }
         });
-        
-        size.add(new GuiButton("reY", but -> {
+        counterDecimal.setExpandableX();
+        counterDecimal.spacing = 2;
+        counterDecimal.align = Align.CENTER;
+        counterDecimal.add(new GuiButton("reY", but -> {
             GuiCounterDecimal sizeXField = get("sizeX", GuiCounterDecimal.class);
             GuiCounterDecimal sizeYField = get("sizeY", GuiCounterDecimal.class);
-            
+
             float y = sizeYField.getValue();
-            
+
             if (frame.display != null)
                 sizeXField.setValue(frame.display.getWidth() / (frame.display.getHeight() / y));
-        }).setTitle(new TextComponent("y->x")));
-        
-        GuiParent flip = new GuiParent(GuiFlow.STACK_X);
-        add(flip);
-        
-        flip.add(new GuiCheckBox("flipX", frame.flipX).setTranslate("gui.waterframes.flipx"));
-        flip.add(new GuiCheckBox("flipY", frame.flipY).setTranslate("gui.waterframes.flipy"));
-        
-        GuiParent align = new GuiParent(GuiFlow.STACK_X);
-        add(align);
-        
-        align.add(new GuiStateButton("posX", frame.min.x == 0 ? 0 : frame.max.x == 1 ? 2 : 1, new TextListBuilder()
+        }).setTitle(new TextComponent("y->x")).setExpandableY());
+
+
+        GuiTable flip_grid = new GuiTable();
+        add(flip_grid);
+        GuiColumn flip_left;
+        GuiColumn flip_right;
+
+        flip_grid.addRow(new GuiRow(flip_left = new GuiColumn(GuiFlow.STACK_X), flip_right = new GuiColumn(GuiFlow.STACK_Y)));
+        flip_grid.align = Align.CENTER;
+        flip_left.align = Align.LEFT;
+        flip_right.align = Align.RIGHT;
+
+        flip_left.add(new GuiStateButton("posX", frame.min.x == 0 ? 0 : frame.max.x == 1 ? 2 : 1, new TextListBuilder()
                 .addTranslated("gui.waterframes.posx.", "left", "center", "right")));
-        align.add(new GuiStateButton("posY", frame.min.y == 0 ? 0 : frame.max.y == 1 ? 2 : 1, new TextListBuilder()
+
+        flip_left.add(new GuiStateButton("posY", frame.min.y == 0 ? 0 : frame.max.y == 1 ? 2 : 1, new TextListBuilder()
                 .addTranslated("gui.waterframes.posy.", "top", "center", "bottom")));
-        
+
+        flip_right.add(new GuiCheckBox("flipX", frame.flipX).setTranslate("gui.waterframes.flipx"));
+        flip_right.add(new GuiCheckBox("flipY", frame.flipY).setTranslate("gui.waterframes.flipy"));
+
+
         GuiTable table = new GuiTable();
+        table.spacing = 2;
         add(table);
         GuiColumn left;
         GuiColumn right;
-        
+
         table.addRow(new GuiRow(left = new GuiColumn(), right = new GuiColumn()));
         left.add(new GuiLabel("t_label").setTitle(new TranslatableComponent("gui.waterframes.rotation").append(":")));
-        right.add(new GuiSlider("rotation", frame.rotation, 0, 360).setExpandableX());
-        
+        right.add(new GuiSlider("rotation", 130, 10, frame.rotation, 0, 360));
+        right.align = Align.RIGHT;
+
         table.addRow(new GuiRow(left = new GuiColumn(), right = new GuiColumn()));
         left.add(new GuiLabel("t_label").setTitle(new TranslatableComponent("gui.waterframes.transparency").append(":")));
-        right.add(new GuiSlider("transparency", frame.alpha, 0, 1).setExpandableX());
-        
+        right.add(new GuiSlider("transparency", 130, 10, frame.alpha, 0, 1));
+        right.align = Align.RIGHT;
+
         table.addRow(new GuiRow(left = new GuiColumn(), right = new GuiColumn()));
         left.add(new GuiLabel("b_label").setTitle(new TranslatableComponent("gui.waterframes.brightness").append(":")));
-        right.add(new GuiSlider("brightness", frame.brightness, 0, 1).setExpandableX());
-        
+        right.add(new GuiSlider("brightness", 130, 10, frame.brightness, 0, 1));
+        right.align = Align.RIGHT;
+
         table.addRow(new GuiRow(left = new GuiColumn(), right = new GuiColumn()));
         left.add(new GuiLabel("d_label").setTitle(new TranslatableComponent("gui.waterframes.distance").append(":")));
-        right.add(new GuiSteppedSlider("distance", frame.renderDistance, 5, 1024).setExpandableX());
-        
+        right.add(new GuiSteppedSlider("distance", 130, 10, frame.renderDistance, 5, 1024));
+        right.align = Align.RIGHT;
+
+
         GuiParent rendering = new GuiParent(GuiFlow.STACK_X);
+        rendering.align = Align.RIGHT;
+        rendering.spacing = 8;
         add(rendering);
-        
         rendering.add(new GuiCheckBox("visibleFrame", frame.visibleFrame).setTranslate("gui.waterframes.visibleFrame"));
         rendering.add(new GuiCheckBox("bothSides", frame.bothSides).setTranslate("gui.waterframes.bothSides"));
-        
-        GuiParent play = new GuiParent(GuiFlow.STACK_X);
-        add(play);
-        
-        play.add(new GuiIconButton("play", GuiIcon.PLAY, button -> PLAY.send(EndTag.INSTANCE)));
-        play.add(new GuiIconButton("pause", GuiIcon.PAUSE, button -> PAUSE.send(EndTag.INSTANCE)));
-        play.add(new GuiIconButton("stop", GuiIcon.STOP, button -> STOP.send(EndTag.INSTANCE)));
-        
-        play.add(new GuiCheckBox("loop", frame.loop).setTranslate("gui.waterframes.loop"));
-        play.add(new GuiLabel("v_label").setTranslate("gui.waterframes.volume"));
-        play.add(new GuiSlider("volume", frame.volume, 0, 1));
-        
+
+        GuiTable volume = new GuiTable();
+        add(volume);
+        GuiColumn volume_left;
+        GuiColumn volume_right;
+
+        volume.addRow(new GuiRow(volume_left = new GuiColumn(GuiFlow.STACK_X), volume_right = new GuiColumn(GuiFlow.STACK_X)));
+        volume_left.add(new GuiLabel("v_label").setTitle(new TranslatableComponent("gui.waterframes.volume").append(":")));
+        volume_right.add(new GuiSlider("volume", 130, 10, frame.volume, 0, 1));
+        volume_right.align = Align.RIGHT;
+
         GuiParent range = new GuiParent(GuiFlow.STACK_X);
         add(range);
-        
-        range.add(new GuiLabel("range_label").setTranslate("gui.waterframes.range"));
-        range.add(new GuiSteppedSlider("range_min", (int) frame.minDistance, 0, 512).setExpandableX());
-        range.add(new GuiSteppedSlider("range_max", (int) frame.maxDistance, 0, 512).setExpandableX());
-        
-        GuiParent bottom = new GuiParent(GuiFlow.STACK_X);
-        bottom.align = Align.RIGHT;
-        add(bottom);
-        save.setEnabled(WaterFrames.CONFIG.canUse(getPlayer(), url.getText()));
-        bottom.add(save);
-        bottom.add(new GuiButton("reload", x -> {
+        range.add(new GuiLabel("range_label").setTitle(new TranslatableComponent("gui.waterframes.range").append(" (min/max):")));
+        range.add(new GuiSteppedSlider("range_min", 63, 10, (int) frame.minDistance, 0, 512).setExpandableX());
+        range.add(new GuiSteppedSlider("range_max", 63, 10, (int) frame.maxDistance, 0, 512).setExpandableX());
+
+        GuiTable playgrid = new GuiTable();
+        add(playgrid);
+        GuiColumn play_left;
+        GuiColumn play_right;
+
+        playgrid.addRow(new GuiRow(play_left = new GuiColumn(GuiFlow.STACK_X), play_right = new GuiColumn(GuiFlow.STACK_X)));
+        play_right.align = Align.RIGHT;
+
+        play_left.add(new GuiIconButton("play", GuiIcon.PLAY, button -> PLAY.send(EndTag.INSTANCE)));
+        play_left.add(new GuiIconButton("pause", GuiIcon.PAUSE, button -> PAUSE.send(EndTag.INSTANCE)));
+        play_left.add(new GuiIconButton("stop", GuiIcon.STOP, button -> STOP.send(EndTag.INSTANCE)));
+        play_left.add(new GuiCheckBox("loop", frame.loop).setTranslate("gui.waterframes.loop"));
+
+        save.setEnabled(FramesConfig.canUse(getPlayer(), url.getText()));
+        play_right.add(save);
+        play_right.add(new GuiButton("reload", x -> {
             synchronized (TextureSeeker.LOCK) {
-                if (Screen.hasShiftDown())
-                    TextureCache.reloadAll();
-                else if (frame.cache != null)
-                    frame.cache.reload();
+                if (Screen.hasShiftDown()) TextureCache.reloadAll();
+                else if (frame.cache != null) frame.cache.reload();
             }
         }).setTranslate("gui.waterframes.reload").setTooltip(new TextBuilder().translate("gui.waterframes.reload.tooltip").build()));
         
