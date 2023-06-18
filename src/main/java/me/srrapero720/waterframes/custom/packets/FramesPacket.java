c473c3832a04b1427cb913b868afeba20e0f9a3c
SrRapero720 <SrRapero720@hotmail.com>
2023-06-18 06:45:09 -0600

    removed unused code

--- src/main/java/me/srrapero720/waterframes/custom/packets/FramesPacket.java
+++ src/main/java/me/srrapero720/waterframes/custom/packets/FramesPacket.java
@@ -38,11 +38,6 @@ public class FramesPacket extends CreativePacket {
     
     @Override
     public void executeServer(ServerPlayer player) {
-        BlockEntity be = player.level.getBlockEntity(pos);
-        if (be instanceof TileFrame frame) {
-            frame.playing = playing;
-            frame.tick = 0;
-        }
     }
 
     @Override
