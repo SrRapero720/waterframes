99fbbd857f99832248bd24cb1e00c5c45eca9b15
SrRapero720 <SrRapero720@hotmail.com>
2023-07-11 22:14:27 -0600

    New configuration file

--- src/main/java/me/srrapero720/waterframes/FramesConfig.java
+++ src/main/java/me/srrapero720/waterframes/FramesConfig.java
@@ -22,63 +22,166 @@ import java.util.*;
 import static me.srrapero720.waterframes.WaterFrames.LOGGER;
 
 public class FramesConfig {
-    public static final Gson GSON = new Gson();
+    private static final Gson GSON = new Gson();
+    private static final Marker IT = MarkerFactory.getMarker("Util");
     private static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
     public static final ForgeConfigSpec SPEC;
 
     // BASIC
-    private static final ForgeConfigSpec.DoubleValue MAX_WIDTH;
-    private static final ForgeConfigSpec.DoubleValue MAX_HEIGHT;
+    private static final ForgeConfigSpec.IntValue MAX_WIDTH;
+    private static final ForgeConfigSpec.IntValue MAX_HEIGHT;
     private static final ForgeConfigSpec.IntValue MAX_RENDER_DISTANCE;
+    private static final ForgeConfigSpec.IntValue MAX_SOUND_DISTANCE;
 
+    private static final ForgeConfigSpec.BooleanValue DISABLE_MASTER_CONTROL;
     private static final ForgeConfigSpec.BooleanValue DISABLE_VIDEOS;
-
     private static final ForgeConfigSpec.BooleanValue DISABLE_ADVENTURE;
     private static final ForgeConfigSpec.BooleanValue DISABLE_SURVIVAL;
     private static final ForgeConfigSpec.BooleanValue DISABLE_PLAYERS;
-
-    private static final ForgeConfigSpec.BooleanValue DISABLE_REDSTONE;
-
     private static final ForgeConfigSpec.BooleanValue DISABLE_WHITELIST;
+
     private static final ForgeConfigSpec.ConfigValue<List<String>> WHITELIST;
 
+    // FRAMES
+    private static final ForgeConfigSpec.BooleanValue DISABLE_REDSTONE;
+
     // PROJECTOR
-    public static final ForgeConfigSpec.IntValue MAX_PROJECTION_DISTANCE;
-    private static final Marker IT = MarkerFactory.getMarker("Util");
+    private static final ForgeConfigSpec.IntValue MAX_PROJECTION_DISTANCE;
+
+    // TV
+    private static final ForgeConfigSpec.IntValue MAX_TV_SAVES;
+    private static final ForgeConfigSpec.IntValue CHANNEL_SWITCH_COOLDOWN;
+
+    // BOOMBOX
+    private static final ForgeConfigSpec.BooleanValue DISABLE_SYNC;
+
+    // UNIVERSAL REMOTE CONTROL
+    private static final ForgeConfigSpec.IntValue MAX_REMOTE_RADIUS;
 
     static {
-        /* waterframes -> */
-        BUILDER.push("waterframes");
+        /* WaterFrames -> */
+        BUILDER.push("WaterFrames");
+        /* WaterFrames -> General */
+        BUILDER.push("General");
 
-        MAX_WIDTH = BUILDER.defineInRange("maxWidth", 100.0D, 10.0D, 1000.0D);
-        MAX_HEIGHT = BUILDER.defineInRange("maxHeight", 100.0D, 10.0D, 1000.0D);
-        MAX_RENDER_DISTANCE = BUILDER.defineInRange("maxRenderDistance", 1000, 10, Integer.MAX_VALUE);
+        MAX_WIDTH = BUILDER
+                .comment("Define max width for all displays in blocks")
+                .defineInRange("maxWidth", 100, 5, 1024);
 
-        DISABLE_ADVENTURE = BUILDER.define("disableUsageAdventure", true);
-        DISABLE_SURVIVAL = BUILDER.define("disableUsageSurvival", false);
-        DISABLE_PLAYERS = BUILDER.define("disableUsageForPlayers", false);
+        MAX_HEIGHT = BUILDER
+                .comment("Define max height for all displays in blocks")
+                .defineInRange("maxHeight", 100, 5, 1024);
 
-        DISABLE_VIDEOS = BUILDER.define("disableVLC", false);
+        MAX_RENDER_DISTANCE = BUILDER
+                .comment(
+                        "Define max rendering distance for all displays in blocks",
+                        "Note: on public multiplayer servers keep this value extremely low, just to prevent FPS drops"
+                )
+                .defineInRange("maxRenderDistance", 1000, 10, 1024);
+        MAX_SOUND_DISTANCE = BUILDER
+                .comment(
+                        "Define max sound distance for all displays in blocks",
+                        "Note: on public multiplayer servers keep this value extremly low, just to prevent global rickrolls or earrapes"
+                )
+                .defineInRange("maxSoundDistance", 100, 5, 1024);
 
-        DISABLE_REDSTONE = BUILDER.comment("Disable pause trigger on redstone signal input").define("disableRedstone", true);
+        DISABLE_MASTER_CONTROL = BUILDER
+                .comment(
+                        "Disables Minecraft master volume can control WF video or audio players volume",
+                        "If is enabled displays volume can't be controlled by Minecraft master volume"
+                )
+                .define("disableMasterVolumeControl", true);
 
-        DISABLE_WHITELIST = BUILDER.define("disableWhitelist", false);
-        WHITELIST = BUILDER.define("whitelist", getJsonListFromRes("whitelist_url.json"));
+        DISABLE_ADVENTURE = BUILDER
+                .comment(
+                        "Disable all WF blocks usage in adventure mode",
+                        "If true, people in adventure mode CAN'T use frames, overwise can use it"
+                )
+                .define("disableOnAdventure", true);
+
+        DISABLE_SURVIVAL = BUILDER
+                .comment(
+                        "Disable all WF blocks usage in survival mode",
+                        "If true, people in survival mode CAN'T use frames, overwise can use it"
+                )
+                .define("disableOnSurvival", false);
+        DISABLE_PLAYERS = BUILDER
+                .comment(
+                        "Disable usage only for admins",
+                        "If true, everyone can use frames, overwise only admins can use it"
+                )
+                .define("disableOnlyAdmins", true);
 
 
-        /* waterframes -> frames */
-        BUILDER.push("frames");
+        DISABLE_VIDEOS = BUILDER
+                .comment(
+                        "Disable VLC usage for all clients connected in your server",
+                        "This disables all video/audio features, but keeps picture and gif features"
+                )
+                .define("disableVideoFeatures", false);
 
-        // waterframes ->
+        DISABLE_WHITELIST = BUILDER
+                .comment(
+                        "Disable whitelist for URLs. when whitelist is enabled and any non listed URL",
+                        "is trying to be added then frame rejects saving. Use this on public servers"
+                )
+                .define("disableWhitelist", false);
+
+        WHITELIST = BUILDER
+                .comment("All permitted URLs")
+                .define("whitelist", getJsonListFromRes("whitelist_url.json"));
+
+        /* WaterFrames -> */
         BUILDER.pop();
 
-        // waterframes -> projector
-        BUILDER.push("projector");
+        /* WaterFrames -> Frames */
+        BUILDER.push("Frames");
+        DISABLE_REDSTONE = BUILDER
+                .comment(
+                        "Disable redstone trigger for frames",
+                        "When is enabled, any powered redstone signal can pause frames",
+                        "if doesn't detect any signal then resume playing"
+                )
+                .define("disableRedstone", false);
+
+        /* WaterFrames -> */
+        BUILDER.pop();
+
+        /* WaterFrames -> Projector */
+        BUILDER.push("Projector");
         MAX_PROJECTION_DISTANCE = BUILDER.comment("Block distance of projection and projector")
                 .defineInRange("maxProjectionDistance", 10, 3, 100);
 
 
-        // waterframes ->
+        /* WaterFrames -> */
+        BUILDER.pop();
+
+        /* WaterFrames -> TV */
+        BUILDER.push("TV");
+        MAX_TV_SAVES = BUILDER.defineInRange("maxTVSaves", 10, 3, 100);
+        CHANNEL_SWITCH_COOLDOWN = BUILDER.defineInRange("channelSwitchCooldown", 3, 1, 10);
+
+
+        /* WaterFrames -> */
+        BUILDER.pop();
+
+        /* WaterFrames -> Boombox */
+        BUILDER.push("Boombox");
+        DISABLE_SYNC = BUILDER
+                .comment(
+                        "Disables passive media sync when multimedia is already playing",
+                        "This option doesn't disable boombox starting/booting sync"
+                )
+                .define("disableSync", false);
+
+        /* WaterFrames -> */
+        BUILDER.pop();
+
+        /* WaterFrames -> Universal Control Remote*/
+        BUILDER.comment("Universal Control Remote").push("UCR");
+        MAX_REMOTE_RADIUS = BUILDER.defineInRange("maxRemoteRadius",  24, 5, 1024);
+
+        // WaterFrames ->
         BUILDER.pop();
 
         // ->
