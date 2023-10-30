920f9da17ba57c956e574e582179ff97b2ea3eb6
SrRapero720 <SrRapero720@hotmail.com>
2023-10-30 02:46:44 -0600

    enhanced collision and interaction shape

--- src/main/java/me/srrapero720/waterframes/common/block/ProjectorBlock.java
+++ src/main/java/me/srrapero720/waterframes/common/block/ProjectorBlock.java
@@ -15,10 +15,13 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.DirectionProperty;
 import net.minecraft.world.level.material.Material;
+import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import team.creative.creativecore.common.gui.GuiLayer;
+import team.creative.creativecore.common.util.math.base.Facing;
+import team.creative.creativecore.common.util.math.box.AlignedBox;
 
 public class ProjectorBlock extends DisplayBlock {
     public static final DirectionProperty FACING = BlockStateProperties.HORIZONTAL_FACING;
@@ -26,9 +29,34 @@ public class ProjectorBlock extends DisplayBlock {
         super(Properties.of(Material.METAL).strength(2.5f, 10.0f).sound(SoundType.METAL).noOcclusion());
     }
 
+    public static @NotNull AlignedBox getBlockBox(Direction direction) {
+        Facing facing = Facing.get(direction);
+        var box = new AlignedBox();
+
+        // fit projector model height
+        box.maxY = 8f / 16f;
+
+        // fit projector thick
+        float blockThickness = 4f / 16f;
+        box.setMin(facing.axis, blockThickness);
+        box.setMax(facing.axis, 1 - blockThickness);
+
+        // fit anchor of it
+        Facing clockWise = Facing.get(direction.getClockWise());
+        box.setMin(clockWise.axis, 1f / 16f);
+        box.setMax(clockWise.axis, 15f / 16f);
+
+        return box;
+    }
+
     @Override
-    public VoxelShape getInteractionShape(@NotNull BlockState state, BlockGetter level, BlockPos pos) {
-        return getBlockBox(state.getValue(getFacing()), 1.0f).voxelShape();
+    public @NotNull VoxelShape getShape(@NotNull BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
+        return getBlockBox(state.getValue(getFacing())).voxelShape();
+    }
+
+    @Override
+    public @NotNull VoxelShape getInteractionShape(@NotNull BlockState state, BlockGetter level, BlockPos pos) {
+        return getBlockBox(state.getValue(getFacing())).voxelShape();
     }
 
     @Override
@@ -50,6 +78,4 @@ public class ProjectorBlock extends DisplayBlock {
     public DirectionProperty getFacing() {
         return FACING;
     }
-
-
 }
