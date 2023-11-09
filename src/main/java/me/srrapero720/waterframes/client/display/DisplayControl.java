5dedb5f80561fe6472fb9173dfd8c50405d2c82d
SrRapero720 <SrRapero720@hotmail.com>
2023-11-08 21:16:09 -0600

    optimized DisplayControl

--- src/main/java/me/srrapero720/waterframes/client/display/DisplayControl.java
+++ src/main/java/me/srrapero720/waterframes/client/display/DisplayControl.java
@@ -2,137 +2,96 @@ package me.srrapero720.waterframes.client.display;
 
 import org.apache.logging.log4j.Marker;
 import org.apache.logging.log4j.MarkerManager;
-import org.slf4j.MarkerFactory;
-
-import java.util.concurrent.atomic.AtomicInteger;
-
-import static me.srrapero720.waterframes.WaterFrames.LOGGER;
 
 public class DisplayControl {
     private static final Marker IT = MarkerManager.getMarker("DisplayControl");
-    public static final Registry REGISTRY = new Registry();
+    static final int SYNC_TIME = Integer.valueOf(1500);
+    static final int DEFAULT_SIZE = Integer.valueOf(32);
 
-    public static final int SYNC_TIME = 1500;
+    private static TextureDisplay[] displays = new TextureDisplay[DEFAULT_SIZE];
+    private static int position = 0;
+    private static boolean checkSize;
 
-    public static void release() { REGISTRY.release(); }
-    public static void add(TextureDisplay display) { REGISTRY.add(display); }
+    public static void add(TextureDisplay display) {
+        synchronized (DisplayControl.class) {
+            if (checkSize) {
+                if (((float) position / displays.length) <= 0.25f) {
+                    TextureDisplay[] freshMeal = new TextureDisplay[displays.length / 2]; // free unused memory
+                    System.arraycopy(displays, 0, freshMeal, 0, position);
+                    displays = freshMeal;
+                }
+                checkSize = false;
+            }
 
-    public static int tickTime = 0;
-    public static void tick() {
-        tickTime++;
-        if (tickTime == Integer.MAX_VALUE) tickTime = 0;
+            if (position >= displays.length) { // position never should be major than length
+                TextureDisplay[] freshMeal = new TextureDisplay[displays.length * 2];
+                position = copyData$resetPosition(displays, freshMeal);
+                displays = freshMeal;
+                checkSize = true;
+            }
+
+            displays[position] = display;
+            position++;
+        }
     }
-
-    public static int getTickTime() { return tickTime; }
 
     public static void pause() {
-        REGISTRY.pauseAll();
+        synchronized (DisplayControl.class) {
+            for (int i = 0; i < position; i++) {
+                if (displays[i] != null) displays[i].pause();
+            }
+        }
     }
 
-    public static class Registry {
-        private static final int DEFAULT_SIZE = 32;
-
-        protected transient TextureDisplay[] data;
-        protected int size = 0;
-        protected boolean nextOpCheckSize = false;
-
-        public Registry() {
-            this.data = new TextureDisplay[DEFAULT_SIZE];
-        }
-
-        public void add(TextureDisplay obj) {
-            if (nextOpCheckSize) {
-                if (((float) size / data.length) <= 0.25f) {
-                    TextureDisplay[] temp = data;
-                    data = new TextureDisplay[temp.length / 2];
-                    System.arraycopy(temp, 0, data, 0, data.length);
-                }
-                nextOpCheckSize = false;
-            }
-
-            if (size == data.length) {
-                TextureDisplay[] temp = data;
-                data = new TextureDisplay[temp.length * 2];
-                size = reduceOf(temp, data);
-                nextOpCheckSize = true;
-            }
-
-            data[size] = obj;
-            size++;
-        }
-
-        public int size() { return size; }
-
-        public void pauseAll() {
-            for (int i = 0; i < size; i++) {
-                if (size >= data.length) break; //FIXME: That wasn't should happen
-                if (data[i] != null) data[i].pause();
+    public void resume() {
+        synchronized (DisplayControl.class) {
+            for (int i = 0; i < position; i++) {
+                if (displays[i] != null) displays[i].resume();
             }
         }
-        public void resumeAll() {
-            for (int i = 0; i < size; i++) {
-                if (size >= data.length) break; //FIXME: That wasn't should happen
-                if (data[i] != null) data[i].resume();
-            }
+    }
+
+    public void remove(int i) {
+        synchronized (DisplayControl.class) {
+            displays[i] = null;
         }
-        public void remove(int i) { data[i] = null; }
-        public void remove(TextureDisplay obj) {
-            if (obj == null) return; // null cannot be removed, duh
-            for (int i = 0; i < size; i++) {
-                if (i >= data.length) break; //FIXME: That wasn't should happen
-                if (obj == data[i]) {
-                    data[i] = null;
+    }
+
+    public void remove(TextureDisplay obj) {
+        if (obj == null) return; // null cannot be removed, duh
+        synchronized (DisplayControl.class) {
+            for (int i = 0; i < position; i++) {
+                if (obj == displays[i]) {
+                    displays[i] = null;
                     break;
                 }
             }
         }
-
-        public void release() {
-            for (int i = 0; i < size; i++) {
-                if (i >= data.length) break;  //FIXME: That wasn't should happen
-                if (data[i] != null) {
-                    data[i].release(false);
-                    data[i] = null;
-                }
-            }
-
-            this.data = new TextureDisplay[DEFAULT_SIZE];
-        }
-
-        static TextureDisplay[] criticalReduceOf(int currentSize, AtomicInteger newSize, TextureDisplay[] originData) {
-            LOGGER.warn(IT, "Size is {} but current data size is {}... preventing a crash!", currentSize, originData.length);
-
-            int size = 0;
-            TextureDisplay[] targetData = new TextureDisplay[originData.length];
-            for (int i = 0; i < originData.length; i++) {
-                if (originData[i] != null) {
-                    targetData[size] = originData[i];
-                    originData[i] = null;
-                    size++;
-                }
-            }
-
-            if (((float) size / targetData.length) < 0.50f) {
-                TextureDisplay[] data = targetData;
-                targetData = new TextureDisplay[targetData.length / 2];
-
-                System.arraycopy(data, 0, targetData, 0, data.length);
-            }
-
-            newSize.set(size);
-            return targetData;
-        }
-
-        static int reduceOf(TextureDisplay[] data, TextureDisplay[] target) {
-            int size = 0;
-            for (int i = 0; i < data.length; i++) {
-                if (data[i] != null) {
-                    target[size] = data[i];
-                    data[i] = null;
-                    size++;
-                }
-            }
-            return size;
-        }
     }
+
+    public static void release() {
+        synchronized (DisplayControl.class) {
+            for (int i = 0; i < position; i++) {
+                if (displays[i] != null) {
+                    displays[i].release(false);
+                    displays[i] = null;
+                }
+            }
+        }
+
+        displays = new TextureDisplay[DEFAULT_SIZE];
+    }
+
+    private static int copyData$resetPosition(TextureDisplay[] current, TextureDisplay[] target) {
+        int freshPosition = 0;
+        for (int i = 0; i < current.length; i++) {
+            if (current[i] != null) {
+                target[freshPosition] = current[i];
+                current[i] = null;
+                freshPosition++;
+            }
+        }
+        return freshPosition;
+    }
+
 }
