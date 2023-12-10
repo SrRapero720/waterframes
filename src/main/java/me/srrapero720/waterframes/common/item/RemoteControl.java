0b25677e309094cd541d62d59747be8680c600c4
SrRapero720 <SrRapero720@hotmail.com>
2023-12-09 19:19:30 -0600

    added remote code to open GUI

--- src/main/java/me/srrapero720/waterframes/common/item/RemoteControl.java
+++ src/main/java/me/srrapero720/waterframes/common/item/RemoteControl.java
@@ -1,14 +1,17 @@
 package me.srrapero720.waterframes.common.item;
 
+import me.srrapero720.waterframes.DisplayConfig;
 import me.srrapero720.waterframes.common.screen.RemoteControlScreen;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import team.creative.creativecore.common.gui.GuiLayer;
+import team.creative.creativecore.common.gui.handler.GuiCreator;
 import team.creative.creativecore.common.gui.handler.ItemGuiCreator;
 
 public class RemoteControl extends Item implements ItemGuiCreator {
@@ -18,7 +21,8 @@ public class RemoteControl extends Item implements ItemGuiCreator {
 
     @Override
     public InteractionResultHolder<ItemStack> use(Level pLevel, Player pPlayer, InteractionHand pUsedHand) {
-        return super.use(pLevel, pPlayer, pUsedHand);
+        if (!pLevel.isClientSide && DisplayConfig.canInteract(pPlayer, pLevel)) GuiCreator.ITEM_OPENER.open(pPlayer, pUsedHand);
+        return InteractionResultHolder.pass(pPlayer.getItemInHand(pUsedHand));
     }
 
     @Override
