ce6d6bd4421889ec01c5946e8d0eee8b30bd1b81
J-RAP <SrRapero720@hotmail.com>
2024-08-19 14:34:52 -0600

    fix crash on server-side

--- src/main/java/me/srrapero720/waterframes/common/screens/DisplayScreen.java
+++ src/main/java/me/srrapero720/waterframes/common/screens/DisplayScreen.java
@@ -158,7 +158,6 @@ public class DisplayScreen extends GuiLayer {
 
         this.reload = new GuiButtonIcon("reload", IconStyles.RELOAD, x -> tile.imageCache.reload());
         this.reload.setTooltip("waterframes.gui.reload");
-        this.reload.setEnabled(enableReload());
         this.save = new GuiButtonIcon("save", IconStyles.SAVE, click -> DisplayNetwork.sendServer(new DataSyncPacket(tile.getBlockPos(), DisplayData.build(this, tile))));
         this.save.setTooltip("waterframes.gui.save");
 
@@ -168,7 +167,6 @@ public class DisplayScreen extends GuiLayer {
                 tile.setPause(true, true);
             });
             this.videoplayer.setTooltip("waterframes.gui.videoplayer");
-            this.videoplayer.setEnabled(enableVideoPlayer());
         } else {
             this.videoplayer = null;
         }
@@ -188,6 +186,13 @@ public class DisplayScreen extends GuiLayer {
         if (!tile.caps.resizes()) {
             this.setDim(WIDTH - 10, HEIGHT - 60);
         }
+
+        this.tick();
+    }
+
+    @Override
+    public boolean isClient() {
+        return tile.isClient();
     }
 
     @Override
@@ -378,10 +383,12 @@ public class DisplayScreen extends GuiLayer {
         }
     }
 
+    @OnlyIn(Dist.CLIENT)
     public boolean enableVideoPlayer() {
         return !tile.data.url.isEmpty() && tile.imageCache != null && tile.imageCache.getStatus() == ImageCache.Status.READY;
     }
 
+    @OnlyIn(Dist.CLIENT)
     public boolean enableReload() {
         return tile.imageCache != null && !this.url.getText().isEmpty() && !tile.data.url.isEmpty() && this.url.getText().equals(tile.data.url);
     }
