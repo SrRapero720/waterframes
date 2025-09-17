d12750aa150b6df6a292b72c7875a508133185a4
J-RAP <SrRapero720@hotmail.com>
2025-09-16 20:20:16 -0600

    Network cleanup

--- src/main/java/me/srrapero720/waterframes/common/network/DisplayNetwork.java
+++ src/main/java/me/srrapero720/waterframes/common/network/DisplayNetwork.java
@@ -3,35 +3,18 @@ package me.srrapero720.waterframes.common.network;
 import me.srrapero720.waterframes.WaterFrames;
 import me.srrapero720.waterframes.common.block.entity.DisplayTile;
 import me.srrapero720.waterframes.common.network.packets.*;
-import net.minecraft.core.BlockPos;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.world.level.Level;
 import team.creative.creativecore.common.network.CreativeNetwork;
-import team.creative.creativecore.common.network.CreativePacket;
 
 import static me.srrapero720.waterframes.WaterFrames.LOGGER;
 
 public class DisplayNetwork {
-    public static final CreativeNetwork NET = new CreativeNetwork(2, LOGGER, new ResourceLocation(WaterFrames.ID, "network"));
+    public static final CreativeNetwork NET = new CreativeNetwork(2, LOGGER, WaterFrames.asResource("network"));
 
-    public static void sendClient(CreativePacket packet, Level level, BlockPos pos) {
-        NET.sendToClient(packet, level, pos);
-    }
-
-    public static void sendClient(CreativePacket packet, ServerPlayer player) {
-        NET.sendToClient(packet, player);
-    }
-
-    public static void sendServer(CreativePacket packet) {
+    public static void sendServer(DisplayDataPacket packet) {
         NET.sendToServer(packet);
     }
 
-    public static void sendClient(DisplayDataPacket packet, DisplayTile tile) {
-        NET.sendToClient(packet, tile.getLevel().getChunkAt(packet.pos));
-    }
-
-    public static void sendServer(DisplayDataPacket packet) {
+    public static void sendServer(DisplayControlPacket packet) {
         NET.sendToServer(packet);
     }
 
@@ -41,9 +24,5 @@ public class DisplayNetwork {
             packet.execute(tile, false);
         }
         NET.sendToClient(packet, tile.getLevel().getChunkAt(packet.pos));
-    }
-
-    public static void sendServer(DisplayControlPacket packet) {
-        NET.sendToServer(packet);
     }
 }
