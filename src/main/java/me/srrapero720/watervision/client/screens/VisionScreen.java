9e3e84bef73904c65e4dbfb6c165944e20bbfb2c
Lunascaped <21148758+Lunascaped@users.noreply.github.com>
2025-09-17 20:40:40 -0500

    fix crash on watervision not being installed

---
+++ src/main/java/me/srrapero720/watervision/client/screens/VisionScreen.java
@@ -0,0 +1,13 @@
+package me.srrapero720.watervision.client.screens;
+
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.network.chat.Component;
+
+import java.net.URI;
+
+public class VisionScreen extends Screen {
+    public VisionScreen(final URI uri, final int volume, final float speed, final boolean stretch, final float gameFadeDuration, final float videoFadeDuration, final boolean controls, final boolean exit)  {
+        super(Component.literal("WaterVision"));
+        throw new UnsupportedOperationException("WaterVision is not installed");
+   }
+}
